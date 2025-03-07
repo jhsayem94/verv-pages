896f037205e9dcdfd3e37a8b47cb36a17bb65549
jhsayem94 <newjhsayem@gmail.com>
2025-03-08 04:12:38 +0600

    About Landlord Page Redesign and Responsive done

--- src/allComponent/Shared/PropertyDemand/PropertyDemand.jsx
+++ src/allComponent/Shared/PropertyDemand/PropertyDemand.jsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/carousel";
 import { useEffect, useState } from "react";
 import PropertyCard from "./PropertyCard";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 const PropertyDemand = () => {
   const [api, setApi] = useState();
   const [current, setCurrent] = useState(0);
@@ -32,7 +33,7 @@ const PropertyDemand = () => {
   }, [api]);
 
   return (
-    <section className="w-[345px] md:w-[1216px] m-auto mt-10 md:mt-[100px]">
+    <section className=" lg:mt-12 mt-8">
       <Carousel
         opts={{
           align: "start",
@@ -40,7 +41,7 @@ const PropertyDemand = () => {
         setApi={setApi}
         className="w-[1216px]"
       >
-        <CarouselContent className="-ml-2">
+        <CarouselContent className="lg:-ml-2">
           {propertyData.map((property) => (
             <CarouselItem
               key={property.id}
@@ -53,19 +54,15 @@ const PropertyDemand = () => {
         {/* <CarouselPrevious /> */}
         {/* <CarouselNext /> */}
       </Carousel>
-      
-      <div className='flex justify-center mt-12 gap-x-4 w-auto'>
-                        <button onClick={() => api?.scrollPrev()} className="w-10 h-10 active:bg-[#50b533] focus:bg-[#50b533] border  border-[#50b533] bg-white rounded-full justify-center items-center inline-flex active:text-white focus:text-white text-[#50b533]">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
-                            </svg>
-                        </button>
-                        <button onClick={() => api?.scrollNext()} className="w-10 h-10 active:bg-[#50b533] focus:bg-[#50b533] border  border-[#50b533] bg-white rounded-full justify-center items-center inline-flex active:text-white focus:text-white text-[#50b533]">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                            </svg>
-                        </button>
-                    </div> 
+
+      <div className='flex justify-center lg:mt-8 mt-6 gap-x-4 w-auto'>
+        <button onClick={() => api?.scrollPrev()} className="w-10 h-10 active:bg-[#50b533] focus:bg-[#50b533] border  border-[#50b533] bg-white rounded-full justify-center items-center inline-flex active:text-white focus:text-white text-[#50b533]">
+          <ArrowLeft className="h-[20px] w-[20px]" />
+        </button>
+        <button onClick={() => api?.scrollNext()} className="w-10 h-10 active:bg-[#50b533] focus:bg-[#50b533] border  border-[#50b533] bg-white rounded-full justify-center items-center inline-flex active:text-white focus:text-white text-[#50b533]">
+          <ArrowRight className="h-[20px] w-[20px]" />
+        </button>
+      </div>
     </section>
   );
 };
