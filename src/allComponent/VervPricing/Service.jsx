0d4a226d94a4dc82f0d9bed098f1036685f57023
jhsayem94 <newjhsayem@gmail.com>
2025-03-09 00:39:18 +0600

    Additional Services Re-design done.

--- src/allComponent/VervPricing/Service.jsx
+++ src/allComponent/VervPricing/Service.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Service = ({ service }) => {
     return (
-        <div key={service.id} className='border border-[#EEF1F3] rounded-lg min-w-[370px]'>
-            <div className='p-5'>
-                <div className='flex justify-between pb-5'>
-                    <h3 className="text-[#30455f] pr-4 text-[20px] font-semibold font-['Inter'] leading-normal ">{service.title}</h3>
+        <div key={service.id} className='border border-[#EEF1F3]  rounded-lg min-w-[370px]'>
+            <div className='grid grid-cols-1 content-between p-5 h-[165px]'>
+                <div className='flex justify-between '>
+                    <h3 className="text-[#30455f] pr-4 w-[230px] text-[20px] font-semibold font-['Inter'] leading-normal ">{service.title}</h3>
                     <div className=''>
                         <h4 className="text-[#56677d] text-[14px] font-normal font-['Inter'] leading-normal" >From</h4>
                         <h6 className="text-center text-[#50b533] text-[24px] font-semibold font-['Inter'] leading-[28.80px]" >{service.price}</h6>
