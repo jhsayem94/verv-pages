0967fa02d4b181fed4cd183411a29af061ab1782
jhsayem94 <newjhsayem@gmail.com>
2025-03-14 03:47:19 +0600

    Rental process Page start

---
+++ src/allComponent/RentalProcess/RentalProcess.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RentalProcess = () => {
+    return (
+        <div>
+            <div className='bg-[#314660] flex justify-center py-11'>
+                <div className='grid grid-cols-1 justify-center gap-y-4'>
+                <h4 className='w-3/5 p-2 mx-auto bg-white/10 rounded-[32px] text-center text-white text-[18px] font-semibold ' >6 bed, 3 bath house</h4>
+                <h1 className="justify-start text-white text-[40px] font-bold font-['Inter'] leading-[48px]">Start the Rental Process</h1>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default RentalProcess;
